d35aaeb4c0899dcf6749addc8e54e0eef9f0d820
Dhruv Sharma <dhruvdattsharma15gmail.com>
2025-01-30 17:42:13 +0530

    initialize React and Vite project with server setup and basic user and product models

---
+++ Server/Src/Model/Product.Model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    categoryId: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const Product = mongoose.model("Product", productSchema )
+
+export { Product }
