e2aef582273104d8bcef276222476f99a6c79bfd
Dhruv Sharma <dhruvdattsharma15gmail.com>
2025-02-01 18:22:39 +0530

    refactor image upload utility; update ImageKit credentials and streamline upload function

--- Server/Src/Utils/UploadFiles.js
+++ Server/Src/Utils/UploadFiles.js
@@ -1,30 +1,23 @@
 import ImageKit from "imagekit";
-import asyncHandler from "./AsyncHandler.js";
 
 let imagekit = new ImageKit({
-	publicKey: 'public_mZQnjRJBFfduq/Rc7ddAsKdyzps=',
-	privateKey: "private_epbZY1XxIPRck1By0GYPDe1+Hco=",
+	publicKey: "public_8s+MM9KN3Wqmh6XUE58DzCou7zw=",
+	privateKey: "private_H1oDsr+Vs/ax8efABlibHKCWawk=",
 	urlEndpoint: "https://ik.imagekit.io/dhruvs/ycom-product-image",
 });
 
 const uploadProductImage = async (file) => {
 	try {
-		const result = await imagekit.upload(
-			{
-				file: file.buffer,
-				fileName: `${Date.now()}-ProductImage`,
-				tags: ["Ycom", "productImage"],
-			},
-			
-		);
+		const result = await imagekit.upload({
+			file: file.buffer,
+			fileName: `${Date.now()}-ProductImage`,
+			tags: ["Ycom", "productImage"],
+		});
 
-		return result.url
+		return result.url;
 	} catch (error) {
-		return null
+		return null;
 	}
 };
 
-export { 
-	uploadProductImage
-}
-
+export { uploadProductImage };
