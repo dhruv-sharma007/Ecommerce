1523a3b89a4eca0a128665edc1638f2af1314864
Dhruv Sharma <dhruvdattsharma15gmail.com>
2025-02-03 01:30:02 +0530

    implement product update, image upload, and deletion functionality; add validation checks and refactor product controller

--- Server/Src/Controller/Product.Controller.js
+++ Server/Src/Controller/Product.Controller.js
@@ -3,6 +3,8 @@ import { Product } from "../Model/Product.Model.js";
 import ApiError from "../Utils/ApiError.js";
 import { ApiResponse } from "../Utils/ApiResponse.js";
 import asyncHandler from "../Utils/AsyncHandler.js";
+import mongoose from "mongoose";
+import check from "../Utils/Check.js"
 
 const addProduct = asyncHandler(async (req, res) => {
 	const { name, price, category, description } = req.body;
@@ -16,7 +18,6 @@ const addProduct = asyncHandler(async (req, res) => {
 	}
 
 	const uploadedImage = await uploadProductImage(req.file);
-	console.log(uploadedImage)
 
 	if (uploadedImage) {
 		const result = await Product.create({
@@ -31,29 +32,87 @@ const addProduct = asyncHandler(async (req, res) => {
 		res.status(201).json(new ApiResponse(201, {}, "Product added succesfully"));
 	}
 
-	res.status(400).json( new ApiError(400, "Error"))
+	res.status(400).json(new ApiError(400, "Error"));
 });
 
-const updateProduct = asyncHandler(async(req, res)=>{
-	const { name, price, category, description } = req.body;
-	
+const updateProduct = asyncHandler(async (req, res) => {
+	const { name, price, category, description, _id } = req.body;
+
 	if (!name && !price && !category && !description) {
 		throw new ApiError(403, "All fields are required");
 	}
 
-	const updateField = {}
+	const updateField = {};
 	if (name) updateField.name = name;
 	if (price) updateField.price = price;
 	if (category) updateField.category = category;
 	if (description) updateField.description = description;
 
-	const seller = req.user?._id
+	if (!mongoose.Types.ObjectId.isValid(_id)) {
+		throw new ApiError(400, "Invalid product ID");
+	}
 
-	await Product.findByIdAndUpdate({seller}, { $set: updateField },{ new: true })  
+	const updatedProduct = await Product.findByIdAndUpdate(
+		_id,
+		{ $set: updateField },
+		{ new: true }
+	);
+
+	if (!updatedProduct) {
+		throw new ApiError(404, "Product not found");
+	}
+
+	res
+		.status(200)
+		.json(new ApiResponse(200, updatedProduct, "Product updated successfully"));
+});
+
+const updateProductImage = asyncHandler(async (req, res) => {
+	const { _id } = req.body;
+	if (!req.file) {
+		throw new ApiError(404, "Product Photo Not Received!");
+	}
+
+	const imageUrl = await uploadProductImage(req.file);
+
+	if (!imageUrl) {
+		throw new ApiError(500, "Image not uploaded");
+	}
+
+	const updatedProduct = await Product.findByIdAndUpdate(
+		_id,
+		{ $set: { imageUrl } },
+		{ new: true }
+	);
+	if (!updatedProduct) {
+		throw new ApiError(404, "Product not found");
+	}
+
+	res
+		.status(200)
+		.json(
+			new ApiResponse(200, updatedProduct, "Product Image updated successfully")
+		);
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+	const { _id } = req.body;
+
+	check(mongoose.Types.ObjectId.isValid(_id), 400, "Invalid product ID")
+
+	const deleteProduct = await Product.findByIdAndDelete(_id, )
+
+	check(deleteProduct, 404, "Product not found")
 
 	res
 	.status(200)
-	.json(new ApiResponse(200, {}, "Product updated successfully"))
-})
+	.json(new ApiResponse(200, deleteProduct, "Product deletef successfully"))
 
-export { addProduct };
+});
+
+export { 
+	addProduct, 
+	updateProduct, 
+	updateProductImage,
+	deleteProduct,
+};
--- Server/Src/Route/Poduct.Route.js
+++ Server/Src/Route/Poduct.Route.js
@@ -1,10 +1,18 @@
-import { addProduct } from "../Controller/Product.Controller.js"
+import { 
+	addProduct, 
+	updateProduct, 
+	updateProductImage,
+	deleteProduct,
+} from "../Controller/Product.Controller.js"
 import express from "express"
 import { upload } from "../Middlewares/Multer.middleware.js"
 import verifySeller from "../Middlewares/Seller.Middleware.js"
 
 const router = express.Router()
 
-router.route("/add-product").post(verifySeller, upload.single("file"), (addProduct))
+router.route("/add-product").post(verifySeller, upload.single("file"), addProduct)
+router.route("/update-product").post(verifySeller, updateProduct)
+router.route("/update-product-image").post(verifySeller, upload.single("file"), updateProductImage)
+router.route("/delete-product").post(verifySeller, deleteProduct)
 
 export default router
